03396a2b0dd3a94fcc3567adefe8c64bd099b7ff
“erhaneth” <gumusucb21@gmail.com>
2022-08-21 10:49:41 -0700

    Update with intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,9 +5,22 @@ export default function intro() {
     <div className="intro" id="intro">
 
       <div className="left">
+        <div className="imgContainer">
+
         <img src="assets/intro.png" alt=""/>
+        </div>
       </div>
-      <div className="right"></div>
+
+      <div className="right">
+        <div className="wrapper">
+          <h2>
+            <span>
+              Merhaba, Ali Gumus
+            </span>       
+          </h2>
+
+        </div>
+      </div>
     </div>
   )
 }
