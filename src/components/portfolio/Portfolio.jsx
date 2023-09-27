375ef337d0d0b63ec5c78270c90ede9eecd029b4
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-09-26 17:47:52 -0700

    add pics

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,33 @@ import "./portfolio.scss";
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio"></div>
-  )
+    <div className="portfolio" id="portfolio">
+      <h2>Hukuki Danışmanlık ve Hizmetlerimiz</h2>
+      <p>
+        ALG Hukuk Ve Danışmanlık Ofisi olarak, hukuki ihtiyaçlarınıza çözüm
+        sunuyoruz. Diyarbakır'ın Yenişehir ilçesinde faaliyet gösteren ofisimiz,
+        uzman kadrosu ve müvekkillerine sunduğu kapsamlı hizmetler ile öne
+        çıkıyor.
+        <br />
+        <br />
+        Hukuki alanlarda uzmanlaşmış ekibimiz, müvekkillerimize pazarlama, iş
+        stratejileri ve hukuki çözümler konusunda destek verirken, yerel ve
+        uluslararası düzeyde faaliyet gösteren müvekkillerimize rehberlik
+        ediyor.
+        <br />
+        <br />
+        Bizimle çalışmanın avantajları:
+        <ul>
+          <li>Alanında uzman avukatlar</li>
+          <li>Pozitif müşteri deneyimi</li>
+          <li>Rekabetçi hukuki çözümler</li>
+          <li>Güvenilirlik ve şeffaflık</li>
+        </ul>
+        <br />
+        ALG Hukuk Ve Danışmanlık Ofisi olarak, müvekkillerimize güçlü iş
+        ilişkileri kurma ve hukuki ihtiyaçlarını en iyi şekilde karşılama
+        taahhüdünde bulunuyoruz. Sizinle çalışmayı dört gözle bekliyoruz.
+      </p>
+    </div>
+  );
 }
