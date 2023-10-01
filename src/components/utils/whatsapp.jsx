8ded3c0d1b8a81b47863bdb6c32b81ed742d4e6f
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-10-01 15:25:00 -0700

    add whatsapp icon

--- src/components/utils/whatsapp.jsx
+++ src/components/utils/whatsapp.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
+import "./whatsapp.scss";
 
 function Whatsapp() {
+  const myNumber = "+905355022106";
   return (
     <div className="whatsapp-icon">
-      <p>Test</p> {/* Adding test text */}
-      <FaWhatsapp size={32} />
+      {/* <span>Haydi konusalim</span> */}
+      <a
+        href={`https://wa.me/${myNumber}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaWhatsapp size={64} color="#25D366" />
+      </a>
     </div>
   );
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,7 +1,36 @@
 import "./works.scss";
 
-export default function Works() {
+import React from "react";
+
+const Works = () => {
   return (
-    <div className="works" id="works"></div>
-  )
-}
+    <div className="services-container">
+      <h1 className="services-header">Services</h1>
+      <hr className="divider" />
+      <p className="services-description">
+        Description about the services offered...
+      </p>
+
+      <div className="images-container">
+        <div className="left-container">
+          <img
+            src="/assets/works/home-practice-areas-business-litigation.jpg"
+            className="large-image"
+          />
+        </div>
+        <div className="right-container">
+          <div className="top-row">
+            <img src="path_to_small_image1.jpg" className="small-image" />
+            <img src="path_to_small_image2.jpg" className="small-image" />
+          </div>
+          <div className="bottom-row">
+            <img src="path_to_small_image3.jpg" className="small-image" />
+            <img src="path_to_small_image4.jpg" className="small-image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
